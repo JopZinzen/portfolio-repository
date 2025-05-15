cc3474886101f8607c5dc05c4cc3e31fe0564e2d
JopZinzen <jopzinzen@icloud.com>
2025-05-15 15:23:40 +0200

    plattegrond toegevoegd met knoppen
    de knoppen zijn nog niet op de zelfde plaats als op de plattegrond, het menu om te reserveren komt pas als op een knop is geklikt.

---
+++ Excellent taste/Plattegrond.js
@@ -0,0 +1,4 @@
+function openFormulier(tafelNummer) {
+    document.getElementById('tafelnummer').textContent = "Tafel " + tafelNummer;
+    document.getElementById('reserveringsformulier').style.display = 'block';
+  }
